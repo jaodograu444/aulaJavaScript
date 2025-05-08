31ac6c0fad41be3dd23aa38862af18901d0f9caa
Joao <joao.lemos.quaros@escola.pr.gov.br>
2025-05-08 11:29:02 -0300

    Trabalhando com Number

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let num1 = Number(prompt("Informe o número 1"));
+let num1 = Number(prompt("Informe o numero 1"));
 let num2 = Number(prompt("Informe o número 2"));
+
 let soma = num1+num2;
-
-
-console.log("Soma: "+soma);
+alert(soma);
